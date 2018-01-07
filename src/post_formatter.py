626cfb3548753305fefecf2bdc1072f83499aff2
Oliver <oliverjrose99@gmail.com>
2018-01-07 20:50:59 +0000

    Don't post caption if its the same as the post title

--- src/post_formatter.py
+++ src/post_formatter.py
@@ -1,4 +1,4 @@
-def post_formatter(link_finder, permalink):
+def post_formatter(link_finder, post):
 
     body = ""
 
@@ -16,12 +16,13 @@ def post_formatter(link_finder, permalink):
         elif link[0] == link_finder.IFRAME:
             body += "[Embedded link, Semi-direct]({})\n\n".format(link[1])
 
-    if link_finder.caption is not None:
+    # don't post caption if its the same as the post title
+    if link_finder.caption is not None and link_finder.caption != post.title:
         body += link_finder.caption + "\n\n"
 
     body += "---\n\n"  # horizontal line
     body += "^(Bot by /u/oliverjrose99) ^| "  # bot ID
     body += "[^About ^& ^Source](https://github.com/oliverjrose99/TumblrDirect) ^| "  # about and source link
-    body += "[^Report ^Issue](http://www.reddit.com/message/compose?to=oliverjrose99&subject=Tumblr+Direct+Issue&message=Link:%20{}%0AAdd%20message%20if%20needed)".format(permalink)  # report issue link
+    body += "[^Report ^Issue](http://www.reddit.com/message/compose?to=oliverjrose99&subject=Tumblr+Direct+Issue&message=Link:%20{}%0AAdd%20message%20if%20needed)".format(post.permalink)  # report issue link
 
     return body
